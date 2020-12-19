19eb0eec7765c0eb1e9b09863be808d59c65808d
arthur <6036916@mail.ru>
2020-12-19 15:43:59 +0300

    add setup procedure

---
+++ setup.py
@@ -0,0 +1,22 @@
+import configparser
+import os
+import argparse
+
+
+def setup():
+    parser = argparse.ArgumentParser(description='Setup initial parameter of smart project')
+    parser.add_argument('-c', default='/etc/ocean_counter.conf', type=str, help='config file name')
+    
+    args = parser.parse_args()
+    
+    config_file = args.c
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    ftp_server = config['ftp server']
+    
+    return (ftp_server.get('ip'), ftp_server.get('login'), ftp_server.get('password'))
+
+
+if __name__ == '__main__':
+    print(setup())
