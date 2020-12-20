a708f87b6682c9cd7ee62a27aa54478c0e5dbc8b
arthur <6036916@mail.ru>
2020-12-20 17:28:50 +0300

    add config file

---
+++ counter_config.py
@@ -0,0 +1,8 @@
+FTP_DIR2 = 'ocean/RU/50/Zhukovskiy/Ocean_Gagarina_67/2nd-floor'
+FTP_DIR1 = 'ocean/RU/50/Zhukovskiy/Ocean_Gagarina_67/1st-floor'
+
+SENSORS = [
+            [101,201,301],
+            [101,201],
+            [103,202]
+           ]
